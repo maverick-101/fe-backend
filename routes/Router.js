e4d98e49269f778e87e318303d0020845ee6ce2c
Yasser Zubair <yasserzubair68@gmail.com>
2019-02-07 00:02:59 +0500

    cors on routes

--- routes/Router.js
+++ routes/Router.js
@@ -1,7 +1,24 @@
 const debug = require("debug-levels")("Router")
 const express = require('express')
+const cors = require('cors');
 
 const app = express()
+  
+const whitelist = ['https://admin.dev.saaditrips.com', 'http://localhost:8080', 'http://localhost:8081'];
+const corsOptions = {
+origin(origin, callback) {
+  console.log('---------------------------------\n origin: ', origin, '\n---------------------------------');
+  if ((whitelist.indexOf(origin) !== -1 || !origin) || (app.get('env') === 'development' && (origin.indexOf('192.168') >= 0 || origin === 'null')) || origin === 'null') {
+    callback(null, true);
+  } else {
+    callback(new Error('Not allowed by CORS'));
+  }
+},
+credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json())
 
 let routerList = [
