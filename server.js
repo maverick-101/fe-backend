7afa94966016bd84b904efa28a84c95048df22a5
Yasser Zubair <yasserzubair68@gmail.com>
2019-02-06 23:41:38 +0500

    enabled cors

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const path = require('path')
 const express = require('express')
 const ModelUtils = require('./models/ModelUtils')
 
+const cors = require('cors');
+
+const whitelist = ['https://admin.dev.saaditrips.com', 'http://localhost:8080', 'http://localhost:8081'];
+const corsOptions = {
+  origin(origin, callback) {
+    console.log('---------------------------------\n origin: ', origin, '\n---------------------------------');
+    if ((whitelist.indexOf(origin) !== -1 || !origin) || (app.get('env') === 'development' && (origin.indexOf('192.168') >= 0 || origin === 'null')) || origin === 'null') {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 // require .env
 require('dotenv').config()
 
