da4dae6e1164fbc90c81219d644671957515b931
Muhammad Ahsan Tariq <ahsan.tatiq34@gmail.com>
2019-02-12 13:58:24 +0500

    rating changes

---
+++ routes/packageRatingApi.js
@@ -0,0 +1,80 @@
+const router = require('express').Router()
+const FeaturedPackage = require('../models/FeaturedPackage')
+const FeaturedPackageLib = require('../lib/FeaturedPackageLib')
+const AppConfig = require('../lib/AppConfig')
+let debug = require("debug-levels")("featuredPackageLibApi")
+
+
+// Saving featuredPackage
+router.post("/save/featuredPackage-save", async (req, res) => {
+  let data = JSON.parse(req.body.featuredPackage)
+  // let data = req.body  // for test on Postman
+	if (!data) {
+    debug.error("ERROR: No Data found in FeaturedPackage request!")
+    res.status(500).send("ERROR: No Data found in FeaturedPackage request!")
+  }
+  let reply = await FeaturedPackageLib.saveFeaturedPackage(data)
+  if (reply) {
+    res.status(200).send('FeaturedPackage Saved!')
+  } else {
+    res.status(500).send('ERROR: Duplicate Field Found or Error Saving FeaturedPackage!')
+  }
+})
+
+// Updating FeaturedPackage
+router.patch("/update/featuredPackage-update", async (req, res) => {
+  let data = JSON.parse(req.body.featuredPackage)
+  // let data = req.body   //for testing in postman
+	if (!data) {
+    debug.error("ERROR: No Data found in FeaturedPackage request!")
+    res.status(500).send("ERROR: No Data found in FeaturedPackage request!")
+  }
+  let reply = await FeaturedPackageLib.updateFeaturedPackage(data)
+  if (reply) {
+    res.status(200).send('FeaturedPackage Updated!')
+  } else {
+    res.status(500).send('ERROR: No ID Found or Error Updating FeaturedPackage!')
+  }
+})
+
+// fetching all FeaturedPackage
+router.get('/fetch/featuredPackage-fetch', async(req, res) => {
+  let reply = await FeaturedPackageLib.fetchAllFeaturedPackages()
+  if (reply) {
+    res.status(200).send(reply)
+  } else {
+    res.status(500).send('ERROR: No FeaturedPackage Found Or Error Fetching FeaturedPackages!')
+  }
+})
+
+// fetching FeaturedPackage by ID
+router.get('/fetchById/featuredPackage-fetchById/:Id', async(req, res) => {
+  let Id = req.params.Id
+  if (!Id) {
+    debug.error("ERROR: No ID found in FeaturedPackage request!")
+    res.status(500).send("ERROR: No ID found in FeaturedPackage request!")
+  }
+  let reply = await FeaturedPackageLib.findFeaturedPackageById(Id)
+  if (reply) {
+    res.status(200).send(reply)
+  } else {
+    res.status(500).send('ERROR: No FeaturedPackage Found Or Error Fetching FeaturedPackage By ID!')
+  }
+})
+
+//Delete FeaturedPackage by ID
+router.delete('/delete/featuredPackage-deleteById/:Id', async(req, res) => {
+  let Id = req.params.Id
+  if (!Id) {
+    debug.error("ERROR: No ID found in FeaturedPackage request!")
+    res.status(500).send("ERROR: No ID found in FeaturedPackage request!")
+  }
+  let reply = await FeaturedPackageLib.deleteFeaturedPackageById(Id)
+  if (reply) {
+    res.status(200).send(reply)
+  } else {
+    res.status(500).send('ERROR: No FeaturedPackage Found Or Deleting FeaturedPackage!')
+  }
+})
+
+module.exports = router
